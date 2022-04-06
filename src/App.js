7435c497d16f14b8aba7759bc3a0f43ffad17fa9
Sandro Vieira <slvieira.quantum@rd.com.br>
2022-04-06 16:54:20 -0300

    Criacao das rotas das pagina da aplicacao

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { Routes, Route, Outlet } from 'react-router-dom';
+import { Routes, Route } from 'react-router-dom';
 
 import Home from './routes/home/home.component';
 import Navigation from './routes/navigation/navigation.component';
